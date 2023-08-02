ffcf9959fe0b843711c6887962876c760e063037
Cristina Tabares <cristina_tabares@hotmail.es>
2023-08-02 16:04:46 +0200

    first commit

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import styled from "@emotion/styled"
+
+const Label = styled.label `
+    color: #fff;
+    display: block;
+    font-family: 'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`
+const Select = styled.select `
+    width: 100%;
+    padding: 14px;
+    border-radius:10px ;
+    font-family: 'Lato', sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    
+`
+
+const useSelectMonedas = (label, opciones) => {
+
+    const [state, setState]= useState('')
+
+  const SelectMonedas = ()=>(
+<>
+    <Label >{label}</Label>
+    <Select
+    value={state}
+    onChange={e => setState(e.target.value)}
+    >
+        <option value=""> Selecciones</option>
+        {opciones.map(opcion=>(
+            <option  value={opcion.id} key={opcion.id} >{opcion.nombre}</option>
+            
+            
+        ))}
+    </Select>
+</>
+
+  )
+    
+  
+  return [state, SelectMonedas]
+}
+
+export default useSelectMonedas
